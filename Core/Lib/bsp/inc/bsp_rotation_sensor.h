38ddb52e5d163faf1a0fc6193ff0abcafef2cf44
Anh Tuan <tuanl799@gmail.com>
2024-08-08 12:30:03 +0700

    style(BSP): Format bsp_rotation_sensor to coding template

--- Core/Lib/bsp/inc/bsp_rotation_sensor.h
+++ Core/Lib/bsp/inc/bsp_rotation_sensor.h
@@ -1,25 +1,70 @@
-/*
- * bsp_adc.h
+
+/**
+ * @file       bsp_rotation_sensor.h
+ * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
+ * @license    This project is released under the Fiot License.
+ * @version    1.0.0
+ * @date       2024-08-05
+ * @author     Toan Nguyen
  *
- *  Created on: Aug 5, 2024
- *      Author: MTC-TTM
+ * @brief      Header file for bsp_rotation_sensor
+ *
+ * @note       None
+ * @example    None
  */
+/* Define to prevent recursive inclusion ------------------------------ */
+#ifndef __BSP_ROTATION_SENSOR_H
+#define __BSP_ROTATION_SENSOR_H
 
-#ifndef LIB_BSP_ROTATION_SENSOR_H_
-#define LIB_BSP_ROTATION_SENSOR_H_
-
+/* Includes ----------------------------------------------------------- */
 #include "stm32f4xx_hal.h"
 
+/* Public defines ----------------------------------------------------- */
 
+/* Public enumerate/structure ----------------------------------------- */
 
-void bsp_adc_start (void);
+/* Public macros ------------------------------------------------------ */
 
-void bsp_adc_stop (void);
+/* Public variables --------------------------------------------------- */
 
-uint32_t bsp_get_adc_value (void);
+/* Public function prototypes ----------------------------------------- */
 
-uint8_t bsp_check_adc_flag (void);
+/**
+ * @brief  Start the ADC conversion with interrupt.
+ *
+ * @attention  This function starts the ADC conversion in interrupt mode.
+ *
+ * @return      void
+ */
+void bsp_adc_start(void);
 
+/**
+ * @brief  Stop the ADC conversion with interrupt.
+ *
+ * @attention  This function stops the ADC conversion in interrupt mode.
+ *
+ * @return      void
+ */
+void bsp_adc_stop(void);
 
+/**
+ * @brief  Get the ADC value.
+ *
+ * @attention  This function retrieves the current ADC value.
+ *
+ * @return      The current ADC value.
+ */
+uint32_t bsp_get_adc_value(void);
 
-#endif /* LIB_BSP_ROTATION_SENSOR_H_ */
+/**
+ * @brief  Check the ADC flag status.
+ *
+ * @attention  This function checks the ADC flag and resets it.
+ *
+ * @return      The ADC flag status.
+ */
+uint8_t bsp_check_adc_flag(void);
+
+#endif /* __BSP_ROTATION_SENSOR_H */
+
+/* End of file -------------------------------------------------------- */
