f4d2e2f99982354591b0895fccc3688e27aea843
Anh Tuan <tuanl799@gmail.com>
2024-08-08 12:37:16 +0700

    style(MW): Format list_song to coding template

--- Core/Lib/middleware/inc/list_song.h
+++ Core/Lib/middleware/inc/list_song.h
@@ -1,60 +1,102 @@
-/*
- * list_song.h
+/**
+ * @file       list_song.h
+ * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
+ * @license    This project is released under the Fiot License.
+ * @version    1.0.0
+ * @date       2024-08-05
+ * @author     Toan Nguyen
  *
- *  Created on: Aug 7, 2024
- *      Author: MTC-TTM
+ * @brief      Header file for list_song
+ *
+ * @note       None
+ * @example    None
  */
+/* Define to prevent recursive inclusion ------------------------------ */
+#ifndef __LIST_SONG_H
+#define __LIST_SONG_H
 
-#ifndef LIB_MIDDLEWARE_INC_LIST_SONG_H_
-#define LIB_MIDDLEWARE_INC_LIST_SONG_H_
-
+/* Includes ----------------------------------------------------------- */
 #include "stddef.h"
 #include "stdint.h"
 
+/* Public defines ----------------------------------------------------- */
+
 // Macros to define song titles and authors
-#define SONG_1_TITLE     "Short_Ghe_qua"
-#define SONG_1_AUTHOR    "Kim_Xuan_cover"
+#define SONG_1_TITLE       "Short_Ghe_qua"
+#define SONG_1_AUTHOR      "Kim_Xuan_cover"
 
-#define SONG_2_TITLE     "Short_River_flows_in_you"
-#define SONG_2_AUTHOR    "Unknown_piano_cover"
+#define SONG_2_TITLE       "Short_River_flows_in_you"
+#define SONG_2_AUTHOR      "Unknown_piano_cover"
 
-#define SONG_3_TITLE     "Short_Noi_Nay_co_anh"
-#define SONG_3_AUTHOR    "Nguyen_Danh_Tu_Guitar_cover"
+#define SONG_3_TITLE       "Short_Noi_Nay_co_anh"
+#define SONG_3_AUTHOR      "Nguyen_Danh_Tu_Guitar_cover"
 
-#define SONG_4_TITLE     "Short_Co_hen_voi_thanh_xuan"
-#define SONG_4_AUTHOR    "Tung_Tic_Guitar_cover"
+#define SONG_4_TITLE       "Short_Co_hen_voi_thanh_xuan"
+#define SONG_4_AUTHOR      "Tung_Tic_Guitar_cover"
 
-#define SONG_5_TITLE     "Short_Khi_con_mo_dan_phai"
-#define SONG_5_AUTHOR    "Tez_Myra_Tran"
+#define SONG_5_TITLE       "Short_Khi_con_mo_dan_phai"
+#define SONG_5_AUTHOR      "Tez_Myra_Tran"
 
-#define SONG_6_TITLE     "Full_Mat_Moc"
-#define SONG_6_AUTHOR    "Ngoc_VAnh_Nhi"
+#define SONG_6_TITLE       "Full_Mat_Moc"
+#define SONG_6_AUTHOR      "Ngoc_VAnh_Nhi"
 
-#define SONG_7_TITLE     "Full_Cho_Toi_Lang_Thang"
-#define SONG_7_AUTHOR    "Ngot_Den"
+#define SONG_7_TITLE       "Full_Cho_Toi_Lang_Thang"
+#define SONG_7_AUTHOR      "Ngot_Den"
 
-#define SONG_8_TITLE     "Full_Con_mua_thang_5"
-#define SONG_8_AUTHOR    "Tran_Lap_Tung_Duong"
+#define SONG_8_TITLE       "Full_Con_mua_thang_5"
+#define SONG_8_AUTHOR      "Tran_Lap_Tung_Duong"
 
-#define SONG_9_TITLE     "Full_Dua_nhau_di_tron"
-#define SONG_9_AUTHOR    "Den_Linh_cao"
+#define SONG_9_TITLE       "Full_Dua_nhau_di_tron"
+#define SONG_9_AUTHOR      "Den_Linh_cao"
 
-#define SONG_10_TITLE    "Full_Dung_lam_trai_tim_anh_dau"
-#define SONG_10_AUTHOR   "Son_Tung"
+#define SONG_10_TITLE      "Full_Dung_lam_trai_tim_anh_dau"
+#define SONG_10_AUTHOR     "Son_Tung"
 
-#define LIST_SONG_QUANTITY  10
+#define LIST_SONG_QUANTITY 10
+/* Public enumerate/structure ----------------------------------------- */
 
-// Structure to access song information
 typedef struct
 {
-    char* song_name;
-    char* author_name;
+  char *song_name;
+  char *author_name;
 } song_info_struct_t;
+/* Public macros ------------------------------------------------------ */
 
+/* Public variables --------------------------------------------------- */
+
+/* Public function prototypes ----------------------------------------- */
+
+/**
+ * @brief  Get the number of songs.
+ *
+ * @attention   This function returns the total number of songs available.
+ *
+ * @return      The number of songs.
+ */
 uint8_t get_list_song_size(void);
 
-char *get_song_name_info (uint8_t index);
+/**
+ * @brief  Get the song name information by index.
+ *
+ * @param[in]   index    The index of the song.
+ *
+ * @attention   This function retrieves the song name information based on the provided index.
+ *
+ * @return      The song name information.
+ */
+char *get_song_name_info(uint8_t index);
 
-char *get_author_name_info (uint8_t index);
+/**
+ * @brief  Get the author name information by index.
+ *
+ * @param[in]   index    The index of the song.
+ *
+ * @attention   This function retrieves the author name information based on the provided index.
+ *
+ * @return      The author name information.
+ */
+char *get_author_name_info(uint8_t index);
 
-#endif /* LIB_MIDDLEWARE_INC_LIST_SONG_H_ */
+#endif /* __LIST_SONG_H */
+
+/* End of file -------------------------------------------------------- */
--- Core/Lib/middleware/src/list_song.c
+++ Core/Lib/middleware/src/list_song.c
@@ -1,50 +1,61 @@
-/*
- * list_song.c
+/**
+ * @file       list_song.c
+ * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
+ * @license    This project is released under the Fiot License.
+ * @version    1.0.0
+ * @date       2024-08-07
+ * @author     Toan Nguyen
  *
- *  Created on: Aug 7, 2024
- *      Author: MTC-TTM
+ * @brief      Source file for list_song
+ *
+ * @note       None
+ * @example    None
  */
 
+/* Includes ----------------------------------------------------------- */
 #include "list_song.h"
 
+/* Private defines ---------------------------------------------------- */
+
+/* Private enumerate/structure ---------------------------------------- */
+
+/* Private macros ----------------------------------------------------- */
+
+/* Public variables --------------------------------------------------- */
+
+/* Private variables -------------------------------------------------- */
 
 // Array of songs
-static song_info_struct_t list_song_index[LIST_SONG_QUANTITY] =
-{
-    {SONG_1_TITLE, SONG_1_AUTHOR},
-    {SONG_2_TITLE, SONG_2_AUTHOR},
-    {SONG_3_TITLE, SONG_3_AUTHOR},
-    {SONG_4_TITLE, SONG_4_AUTHOR},
-    {SONG_5_TITLE, SONG_5_AUTHOR},
-    {SONG_6_TITLE, SONG_6_AUTHOR},
-    {SONG_7_TITLE, SONG_7_AUTHOR},
-    {SONG_8_TITLE, SONG_8_AUTHOR},
-    {SONG_9_TITLE, SONG_9_AUTHOR},
-    {SONG_10_TITLE, SONG_10_AUTHOR}
-};
+static song_info_struct_t list_song_index[LIST_SONG_QUANTITY] = {
+{SONG_1_TITLE, SONG_1_AUTHOR},  {SONG_2_TITLE, SONG_2_AUTHOR}, {SONG_3_TITLE, SONG_3_AUTHOR},
+{SONG_4_TITLE, SONG_4_AUTHOR},  {SONG_5_TITLE, SONG_5_AUTHOR}, {SONG_6_TITLE, SONG_6_AUTHOR},
+{SONG_7_TITLE, SONG_7_AUTHOR},  {SONG_8_TITLE, SONG_8_AUTHOR}, {SONG_9_TITLE, SONG_9_AUTHOR},
+{SONG_10_TITLE, SONG_10_AUTHOR}};
+/* Private function prototypes ---------------------------------------- */
+
+/* Function definitions ----------------------------------------------- */
 
 // Function to get the number of songs
-uint8_t get_list_song_size(void)
-{
-    return LIST_SONG_QUANTITY;
-}
+uint8_t get_list_song_size(void) { return LIST_SONG_QUANTITY; }
 
 // Function to get song information by  index
-char *get_song_name_info (uint8_t index)
+char *get_song_name_info(uint8_t index)
 {
   uint8_t struct_index = index - 1;
 
-  char *song_name_info =  list_song_index[struct_index].song_name;
+  char *song_name_info = list_song_index[struct_index].song_name;
 
   return song_name_info;
 }
 
-char *get_author_name_info (uint8_t index)
+char *get_author_name_info(uint8_t index)
 {
   uint8_t struct_index = index - 1;
 
-  char *author_name_info =  list_song_index[struct_index].author_name;
+  char *author_name_info = list_song_index[struct_index].author_name;
 
   return author_name_info;
 }
+/* Private definitions ----------------------------------------------- */
 
+/* End of file -------------------------------------------------------- */
